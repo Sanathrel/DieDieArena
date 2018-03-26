1adc375eefbc874bf754429f398d2c10dfb2f90a
Luna <lunar.light@bk.ru>
2018-03-26 11:28:48 +0300

    Adding DieDieArena files to Git.

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float fireRate;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    private float nextFire;
+    private Vector3 objectPosition;
+
+    private void Update()
+    {
+        objectPosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(objectPosition.y, objectPosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 20, Vector3.forward);
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }        
+    }
+}
